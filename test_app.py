5eadd336513e344c750f053bd1b44cb659f0c1ac
CDFER <cdirksfer@gamil.com>
2024-12-16 16:05:26 +1300

    try again...

--- test_app.py
+++ test_app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/test')
+def test():
+    return 'Hello, World!'
+
 # Create a WSGI callable object that wraps the app instance
 def run_app():
     return app
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-# wsgi.py
-from test_app import app
-
-if __name__ == '__main__':
-    app.run()
